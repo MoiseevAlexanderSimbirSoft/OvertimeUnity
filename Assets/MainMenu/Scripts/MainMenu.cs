d33f670e6924b3672635d7f62f8c2ab5bec028a4
Сергей Катаев <sergejkataev@MacBook-Air-Sergej.local>
2024-09-08 00:06:08 +0300

    menu

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
-    public void SelectSamara()
+    public void navigate(string targetScene)
     {
-        SceneManager.LoadSceneAsync("Assets/Scenes/SamaraOffice.unity");
-    }
+        SceneManager.LoadSceneAsync(targetScene);
+    } 
 }
