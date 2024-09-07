18a0d452ef008a33cbfb954418a55bb45674c1e2
AlexanderMoiseev <aleksandr.moiseev@simbirsoft.com>
2024-09-07 21:13:36 +0300

    Serega AI

---
+++ Assets/Serega/Serega_AI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+
+public class Serega_AI : MonoBehaviour
+{
+    private NavMeshAgent AI_Agent;
+    private GameObject Player;
+    public GameObject Panel_GameOver;
+    private TextMeshProUGUI gameOverText;
+
+    void Start()
+    {
+        AI_Agent = gameObject.GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        gameOverText = Panel_GameOver.GetComponentInChildren<TextMeshProUGUI>();
+    }
+    
+    void FixedUpdate()
+    {
+        AI_Agent.SetDestination(Player.transform.position);
+        float Dist_Player = Vector3.Distance(Player.transform.position, gameObject.transform.position);
+        if(Dist_Player < 0.5)
+        {
+            Player.SetActive(false);
+            gameOverText.text = "Серега оказался быстрее";
+            Panel_GameOver.SetActive(true);
+        }
+    }
+}
